f6dec282c161dd6f05ffe551d85761fe53685282
Luis Felipe Vanegas <yo@felipevanegas.com>
2017-03-15 17:30:53 +0000

    Se organiza la página de detalle y se hace un aleatorio de libros sugerios

--- assets/php/contenidos/detalle.php
+++ assets/php/contenidos/detalle.php
@@ -5,17 +5,14 @@ require_once(SYS_PATH_PHP . "Querys.php");
 
 $q = new Querys();
 $l = $q->detLibro($_GET["menu"]);
+$s = $q->sugeridos($_GET["menu"]);
 
 if($l){
-    $c = 0;
     $html = '<section class="row">';
     foreach($l as $row_l){
-        if($c%4 == 0){
-            $html .= '<div class="row">';
-        }
-       $html .= '<article class="col-md-3 libro__info">';
-       $html .= '<a href="/' . $row_l["url"] . '">';
-       $html .= "<figure class='libro__img'>";
+       $html .= '<article class="col-md-12 libro__info">';
+       $html .= "<figure class='libro__img col-md-4
+       '>";
        if(!empty($row_l["caratula"])){
            if(file_exists("imagenes/".$row_l["caratula"])){
                $html .= '<img src="imagenes/'.$row_l["caratula"].'" width="50" height="50">';
@@ -26,20 +23,40 @@ if($l){
            $html .= '<img src="imagenes/libro.jpeg" width="50" height="50">';
        }
        $html .= '</figure>';
+       $html .= '<div class="col-md-8">';
         $html .= (!empty($row_l["libro"])) ? "<h3>" . $row_l["libro"] . "</h3>" : "";
-        $html .= "</a>";
         $html .= (!empty($row_l["autor"])) ? "<p><em>".$row_l["autor"] . "</em></p>" : "";
         $html .= (!empty($row_l["fecha"])) ? "<p>".$row_l["fecha"] . "</p>" : "";
         $html .= (!empty($row_l["genero"])) ? "<p>".$row_l["genero"]  . "</p>" : "";
         $html .= (!empty($row_l["editorial"])) ? "<p><strong>".$row_l["editorial"] . "</strong></p>" : "";
         $html .= (!empty($row_l["precio"])) ? "<p> $ ".$row_l["precio"] . "</p>" : "";
+        $html .= (!empty($row_l["inventario"])) ? "<p>".$row_l["inventario"] . "</p>" : "";
+        $html .= (!empty($row_l["codigo"])) ? "<p>".$row_l["codigo"] . "</p>" : "";
+        $html .= (!empty($row_l["descripcion"])) ? "<p>".nl2br($row_l["descripcion"]) . "</p>" : "";
+        $html .= "</div>";
        $html .= '</article>';
-       $c++;
-       if($c%4 == 0){
-          $html .= "</div>"; 
-       }
     }
     $html .= '</section>';
+    
+    if($s){
+        $html .= "<div class='co-md-12'>";
+        foreach($s as $row_s){
+            $html .= "<figure class='col-md-2'>";
+        if(!empty($row_s["caratula"])){
+           if(file_exists("imagenes/".$row_s["caratula"])){
+               $html .= '<img src="imagenes/'.$row_s["caratula"].'" width="50" height="50">';
+           } else {
+               $html .= '<img src="imagenes/libro.jpeg" width="50" height="50">';
+           }
+       } else {
+           $html .= '<img src="imagenes/libro.jpeg" width="50" height="50">';
+       }
+       $html .= "<figcaption>".$row_s["libro"]."</figcaption>";
+       $html .= "</figure>";
+        }
+        
+        $html .= "</div>";
+    }
     
         
     
--- assets/php/system/Querys.php
+++ assets/php/system/Querys.php
@@ -33,7 +33,20 @@ class Querys
     
     public function detLibro($p = array()) {
         $pa = is_array($p) ? $p : array($p);
-        $id = $this->conn->consulta("SELECT id_libro, libro, fecha, caratula, precio,  autor, genero, editorial, url FROM libros INNER JOIN autores ON (id_autor = autor_id) INNER JOIN generos ON (id_genero = genero_id) INNER JOIN editoriales ON (id_editorial = editorial_id) WHERE url IN (?) AND estado IN (1) ORDER BY libro ASC LIMIT 1;", $pa);
+        $id = $this->conn->consulta("SELECT id_libro, libro, fecha, caratula, precio,  autor, genero, editorial, url, inventario, paginas, descripcion, codigo FROM libros INNER JOIN autores ON (id_autor = autor_id) INNER JOIN generos ON (id_genero = genero_id) INNER JOIN editoriales ON (id_editorial = editorial_id) WHERE url IN (?) AND estado IN (1) ORDER BY libro ASC LIMIT 1;", $pa);
+        if($id) {
+            return $id;
+        } else {
+            return false;
+        }
+
+        $this->conn = NULL;
+        exit();
+    }
+    
+    public function sugeridos($p = array()) {
+        $pa = is_array($p) ? $p : array($p);
+        $id = $this->conn->consulta("SELECT libro, caratula FROM libros WHERE url NOT IN (?) AND estado IN (1) ORDER BY RAND() LIMIT 6;", $pa);
         if($id) {
             return $id;
         } else {
